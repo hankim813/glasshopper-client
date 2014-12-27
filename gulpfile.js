553d4ace7d683b49ae66c26043ecb0ed4aff48e5
Stefano Schiavi <stefano@bvprojects.org>
2014-12-26 19:15:23 -0800

    restructe ionic directories, add gulp tasks
    - followed the instructijons at: http://goo.gl/fJxZUQ
    - all working! YES! need to figure out html2js

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,141 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
+var cache = require('gulp-cached');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var ngHtml2Js = require('gulp-ng-html2js');
+var minifyHtml = require('gulp-minify-html');
+var angularFilesort = require('gulp-angular-filesort');
+var bowerFiles = require('main-bower-files');
 var sh = require('shelljs');
 
+var bowerConf = {
+        paths: './',
+        includeDev: true
+    };
+
+var htmlminOpts = {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeEmptyAttributes: false,
+        collapseBooleanAttributes: true,
+        removeRedundantAttributes: true
+    };
+
+var smConf = {includeContent: false, sourceRoot: '../src'};
+
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  js: ['./src/**/*.js'],
+  vendor: ['./vendor/**/*.js'],
+  templates: ['./src/**/*.tpl.html']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'js', 'vendor', 'templates', 'fonts']);
+
+
+/*
+ | --- SASS -----------------------------------------------
+ */
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
+    .pipe(cache('css'))
+    .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sass())
+        //.pipe(sourcemaps.write('./maps'))
+        //.pipe(gulp.dest('./www/css/'))
+        .pipe(minifyCss({
+          keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+    .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
 
+/*
+ | --- JS -------------------------------------------------
+ */
+
+gulp.task('vendor', function(done) {
+    gulp.src(bowerFiles(bowerConf))
+        .pipe(cache('vendor'))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        //.pipe(angularFilesort())
+        .pipe(concat('vendor.js'))
+        //.pipe(ngAnnotate())
+        //.pipe(sourcemaps.write('./maps'))
+        //.pipe(gulp.dest('./www/js/'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('./www/js/'))
+        .on('end', done);
+});
+
+gulp.task('js', function(done) {
+    gulp.src(['./src/**/*.js'])
+        .pipe(cache('app'))
+        .pipe(angularFilesort())
+        .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(concat('all.js'))
+            .pipe(ngAnnotate())
+        //.pipe(sourcemaps.write('./maps'))
+        //.pipe(gulp.dest('./www/js/'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('./www/js/'))
+        .on('end', done);
+});
+
+/*
+ | --- Templates ------------------------------------------
+ */
+
+gulp.task('templates', function(done) {
+    gulp.src('./src/**/*.tpl.html')
+        .pipe(cache('templates'))
+        .pipe(minifyHtml({
+            empty: true,
+            spare: true,
+            quotes: true
+        }))
+        .pipe(ngHtml2Js({
+            moduleName: 'templates'
+        }))
+        .pipe(concat('templates.js'))
+        .pipe(ngAnnotate())
+        //.pipe(gulp.dest('./www/js/'))
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./www/js/'))
+        .on('end', done);
+});
+
+/*
+ | --- Fonts ------------------------------------------
+ */
+
+gulp.task('fonts', function() {
+    return gulp.src([
+        './vendor/ionic/**/*.ttf', './vendor/ionic/**/*.woff', './vendor/ionic/**/*.svg',
+        './vendor/fontawesome/**/*.ttf', './vendor/fontawesome/**/*.woff', './vendor/fontawesome/**/*.svg'
+    ])
+        .pipe(gulp.dest('./www/assets'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.js, ['js']);
+  gulp.watch(paths.vendor, ['vendor']);
+    gulp.watch(paths.templates, ['templates']);
 });
 
 gulp.task('install', ['git-check'], function() {
