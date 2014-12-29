2eb61644e1ebfcba5a14071b6b4aacbe63b971c9
Stefano Schiavi <stefano@bvprojects.org>
2014-12-28 23:38:36 -0800

    refactor createuser and dologin callback functions
    - extracted success callback
    - extracted error callback

--- src/login/loginCtrl.js
+++ src/login/loginCtrl.js
@@ -15,39 +15,36 @@ angular.module('loginCtrl', [])
   $scope.createUser = function() {
     $http
       .post('http://127.0.0.1:3000/api/signup', $scope.loginData)
-      .success(function (data, status, headers, config) {
-        console.log("oh yeesssss")
-        $localStorage.token = data.token;
-        $localStorage.user = data.user;
-      })
-      .error(function (data, status, headers, config) {
-        // Erase the token if the user fails to log in
-        console.log("oh nooooo")
-        delete $localStorage.token;
-        delete $localStorage.user;
-        // Handle login errors here
-        $scope.message = data.message;
-      });
+      .success(authSuccessCallback)
+      .error(authErrorCallback);
   };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     UserAuthFactory.login($scope.loginData)
 
-    .success(function (data) {
-      $localStorage.token = data.token;
-      $localStorage.user = data.user;
-      AuthenticationFactory.isLogged = true;
-      $location.path("/app/playlists");
-    })
+    .success(authSuccessCallback)
+    .error(authErrorCallback);
+  }
 
-    .error(function (data, status, headers, config) {
-      // Erase the token if the user fails to log in
-      delete $localStorage.token;
-      delete $localStorage.user;
-      // Handle login errors here
 
-      $scope.message = data.message;
+  function authSuccessCallback (data) {
+    $localStorage.token = data.token;
+    $localStorage.user = data.user;
+    AuthenticationFactory.isLogged = true;
+    $ionicHistory.nextViewOptions({
+        disableAnimate: true,
+        disableBack: true
     });
+    $location.path("/app/playlists");
+  }
+
+  function authErrorCallback (data, status, headers, config) {
+    // Erase the token if the user fails to log in
+    delete $localStorage.token;
+    delete $localStorage.user;
+    // Handle login errors here
+
+    $scope.message = data.message;
   }
 });
