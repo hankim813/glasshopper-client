a0b08d9f551905c5b4bd06998209f327f6d59494
Stefano Schiavi <stefano@bvprojects.org>
2014-12-28 22:41:51 -0800

    extract logincontroller logic to factories

--- src/login/loginCtrl.js
+++ src/login/loginCtrl.js
@@ -1,32 +1,53 @@
 angular.module('loginCtrl', [])
 
-.controller('LoginController', function($scope, $http, $localStorage) {
+.controller('LoginController', function($scope, $http, $location, $localStorage, $ionicHistory, UserAuthFactory, AuthenticationFactory) {
+
+  if(AuthenticationFactory.isLogged) {
+    $ionicHistory.nextViewOptions({
+        disableAnimate: true,
+        disableBack: true
+    });
+    $location.path("/app/playlists");
+  }
   // Form data for the login modal
   $scope.loginData = {};
 
   $scope.createUser = function() {
-    console.log('Doing register', $scope.loginData);
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('hello Han')
     $http
-      .post('http://127.0.0.1:3000/api/signin', $scope.loginData)
+      .post('http://127.0.0.1:3000/api/signup', $scope.loginData)
       .success(function (data, status, headers, config) {
+        console.log("oh yeesssss")
         $localStorage.token = data.token;
         $localStorage.user = data.user;
-        console.log('logged in');
-        console.log($localStorage.user);
-        console.log($localStorage.token);
       })
       .error(function (data, status, headers, config) {
         // Erase the token if the user fails to log in
+        console.log("oh nooooo")
         delete $localStorage.token;
         delete $localStorage.user;
-        console.log('error!');
         // Handle login errors here
         $scope.message = data.message;
       });
   };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    UserAuthFactory.login($scope.loginData)
+
+    .success(function (data) {
+      $localStorage.token = data.token;
+      $localStorage.user = data.user;
+      AuthenticationFactory.isLogged = true;
+      $location.path("/app/playlists");
+    })
+
+    .error(function (data, status, headers, config) {
+      // Erase the token if the user fails to log in
+      delete $localStorage.token;
+      delete $localStorage.user;
+      // Handle login errors here
+
+      $scope.message = data.message;
+    });
+  }
 });
