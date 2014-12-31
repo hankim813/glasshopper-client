4bd53b794702e7aff68d69b886533a9c92e6931e
Stefano Schiavi <stefano@bvprojects.org>
2014-12-30 19:41:57 -0800

    add connect with facebook
    - added facebookLogin function
    - added fbSuccessCallback function

--- src/login/loginCtrl.js
+++ src/login/loginCtrl.js
@@ -1,13 +1,19 @@
 angular.module('loginCtrl', [])
 
-.controller('LoginController', function($scope, $http, $location, $localStorage, $ionicHistory, UserAuthFactory, AuthenticationFactory) {
+.controller('LoginController', function($scope, $http, $location, $localStorage, $ionicHistory, $cordovaOauth, $ionicLoading, UserAuthFactory, AuthenticationFactory, UserProfileFactory) {
 
   // Form data for the login modal
   $scope.loginData = {};
+  $scope.profileData = {};
 
   $scope.createUser = function() {
-    $http
-      .post('http://127.0.0.1:3000/api/signup', $scope.loginData)
+    UserProfileFactory.create($scope.profileData)
+      .success(authSuccessCallback)
+      .error(authErrorCallback);
+  };
+
+  $scope.fbCallback = function() {
+    UserProfileFactory.fbCallback($scope.profileData)
       .success(authSuccessCallback)
       .error(authErrorCallback);
   };
@@ -15,21 +21,43 @@ angular.module('loginCtrl', [])
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     UserAuthFactory.login($scope.loginData)
-
-    .success(authSuccessCallback)
-    .error(authErrorCallback);
+      .success(authSuccessCallback)
+      .error(authErrorCallback);
   };
 
+  $scope.facebookLogin = function() {
+    console.log("in the function")
+      $cordovaOauth.facebook("196511150372923", ["public_profile", "email"]).then(fbSuccessCallback, fbErrorCallback);
+  }
+
+  function fbSuccessCallback (result) {
+    $localStorage.fbToken              = result.access_token;
+
+    $ionicLoading.show();
+
+    UserProfileFactory.getFacbookProfile();
+
+    $scope.profileData.profilePhotoUrl = $localStorage.profilePhotoUrl;
+    $scope.profileData.facebook        = $localStorage.facebook;
+    $scope.profileData.facebook.token  = $localStorage.fbToken;
+
+    $scope.fbCallback();
+    $ionicLoading.hide();
+  }
+
+  function fbErrorCallback (error) {
+    alert(error);
+  }
 
   function authSuccessCallback (data) {
     $localStorage.token = data.token;
     $localStorage.user = data.user;
     AuthenticationFactory.isLogged = true;
     $ionicHistory.nextViewOptions({
-        disableAnimate: true,
         disableBack: true
     });
     $scope.loginData = {};
+    $scope.profileData = {};
     $location.path("/app/playlists");
   }
 
@@ -39,6 +67,6 @@ angular.module('loginCtrl', [])
     delete $localStorage.user;
     // Handle login errors here
 
-    $scope.message = data.message;
+    alert(data.message);
   }
 });
