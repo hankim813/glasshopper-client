cd11713c00a2c91fe097248286d880e1fd5d5a65
neilcode <njlambert@gmail.com>
2015-01-02 02:08:19 -0800

    refactors bar-dashboard to have a factory for faking data

--- src/app.js
+++ src/app.js
@@ -4,17 +4,17 @@
 // 'glassHopper' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'glassHopper.controllers' is found in controllers.js
-angular.module('glassHopper', ['ionic', 'ngCordova', 'ngStorage', 'templates', 'glassHopper.controllers','loginRoutes', 'loginCtrl', 'authFactories'])
+angular.module('glassHopper', ['ionic', 'ngCordova', 'ngStorage', 'templates', 'glassHopper.controllers','loginRoutes', 'loginCtrl', 'authFactories', 'barDashboard.services'])
 
 
 .run(function($rootScope, $ionicPlatform, $cordovaSplashscreen, $location, $ionicHistory, AuthenticationFactory) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
+if (window.cordova && window.cordova.plugins.Keyboard) {
+  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+}
+if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
@@ -81,15 +81,15 @@ angular.module('glassHopper', ['ionic', 'ngCordova', 'ngStorage', 'templates', '
       }
     }
   })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
+  .state('app.playlists', {
+    url: "/playlists",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlists.html",
+        controller: 'PlaylistsCtrl'
       }
-    })
+    }
+  })
 
   .state('app.single', {
     url: "/playlists/:playlistId",
@@ -99,6 +99,11 @@ angular.module('glassHopper', ['ionic', 'ngCordova', 'ngStorage', 'templates', '
         controller: 'PlaylistCtrl'
       }
     }
+  })
+  .state('barView', {
+    url: "/bar-dash",
+    templateUrl: "bar-dashboard/barView.tpl.html",
+    controller: 'barController'
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
--- src/bar-dashboard/barCtrl.js
+++ src/bar-dashboard/barCtrl.js
@@ -1,32 +1,10 @@
 angular.module('barCtrl', [])
 
-.controller('barController', function($scope, $http, $location, $localStorage, $ionicHistory, $cordovaOauth, $ionicLoading, UserAuthFactory, AuthenticationFactory, UserProfileFactory){
-
-	//retrieve a Bar model object
-	//name, address, photoUrl, avgPrice (between 1 - 5), location (array with long and lat values)
-	//createdAt, updatedAt
-
+.controller('barController', function($scope, $http, $location, $localStorage, $ionicHistory, $cordovaOauth, $ionicLoading, UserAuthFactory, AuthenticationFactory, UserProfileFactory, BarDashboardFactory){
 	//for now, we use stub data
-	$scope.barData = {
-		name    : "Blackthorn",
-	  address : "1234 Fleet Street, San Francisco CA 90029",
-	  photoUrl: "http://cdn.partyearth.com/photos/a24c7146df1ffac2eb3249d542c2ce7f/blackthorn-tavern_s345x230.jpg?1375139690",
-	  avgPrice: 2,
-	  location: ""
-	};
+	$scope.bar = BarDashboardFactory.get(0) //grab the first fake bar
 
-	//eventually replace stub data with this API call
-	$scope.retrieveBarData = function(){
-		$http.get('/fetch', {
-			params: {latLng: "37.7631823,-122.4659945"}
-		}).
-			success(function(data){
-				$scope.barData = data;
-			}).
-			error(function(){
-				console.log("could not retrieve bar data");
-			});
-	};
+	
 
 
 });
