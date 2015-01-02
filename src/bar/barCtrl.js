886653d2c4a74af622602976915741356f35863c
Stefano Schiavi <stefano@bvprojects.org>
2015-01-02 13:20:48 -0800

    add bar show
    - added single bar template
    - added BarSingleController
    - added get method to barFactory
    - added bar route to barRoutes

--- src/bar/barCtrl.js
+++ src/bar/barCtrl.js
@@ -4,4 +4,9 @@ controller('BarController', function($scope, $http, $location, $ionicHistory, $l
 
   $scope.bars = bars;
 
+}).
+controller('BarSingleController', function($scope, $http, $location, $ionicHistory, $localStorage, $ionicLoading, barFactory, bar){
+
+  $scope.bar = bar;
+
 });
