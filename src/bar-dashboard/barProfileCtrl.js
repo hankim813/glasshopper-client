852b40e68d5f5d062a90e0281d2aef044ff6c377
Han Kim <hankim813@gmail.com>
2015-01-03 15:52:47 -0800

    moves bar-dashboard files into bar dir, deletes unneeded templates

--- src/bar-dashboard/barProfileCtrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('barDashCtrl', [])
-
-.controller('barDashController', function($scope, $http, $location, $localStorage, $ionicHistory, $cordovaOauth, $ionicLoading, UserAuthFactory, AuthenticationFactory, UserProfileFactory){
-	//for now, we use stub data
-	$scope.bar = {
-		name: "neil's bar"
-	}
-
-});
