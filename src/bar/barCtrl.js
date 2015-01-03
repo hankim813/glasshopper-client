0200bd4270cd29a55a6a160eb238f500782819a2
Sharmila Jesupaul <tosharmila@gmail.com>
2015-01-02 18:27:16 -0800

    Add modal logic to bar controlle and add review button to bar dashboard

--- src/bar/barCtrl.js
+++ src/bar/barCtrl.js
@@ -1,12 +1,28 @@
 angular.module('barCtrl', []).
 
 controller('BarController', function($scope, $http, $location, $ionicHistory, $localStorage, $ionicLoading, barFactory, bars){
-
+  
   $scope.bars = bars;
 
 }).
-controller('BarSingleController', function($scope, $http, $location, $ionicHistory, $localStorage, $ionicLoading, barFactory, bar){
+controller('BarSingleController', function($scope, $ionicModal, $http, $location, $ionicHistory, $localStorage, $ionicLoading, barFactory, bar){
+  console.log($scope);
 
   $scope.bar = bar;
 
+
+  // Create the review modal
+  $ionicModal.fromTemplateUrl('review/review.tpl.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.reviewBar = function() {
+    $scope.modal.show();
+  };
+
+  $scope.closeReview = function() {
+    $scope.modal.hide();
+  };
 });
