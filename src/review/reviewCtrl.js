9e4d94b6f934ec6bc1400683548fb707317e22b3
Sharmila Jesupaul <tosharmila@gmail.com>
2015-01-04 15:46:37 -0800

    Bug Fix and Clean-Up: - adds client side validation to crowdLevel and avgAge button selection - cleans up review controller indentation

--- src/review/reviewCtrl.js
+++ src/review/reviewCtrl.js
@@ -1,85 +1,106 @@
 angular.module('reviewCtrl', [])
-.controller('ReviewController', ['$scope', '$ionicModal', '$http', '$location', '$localStorage', '$ionicHistory', '$cordovaOauth', '$ionicLoading', 'reviewFactory', function($scope, $ionicModal, $http, $location, $localStorage, $ionicHistory, $cordovaOauth, $ionicLoading, reviewFactory){
-  $scope.Math           = window.Math;
-  $scope.rawData        = {};
-  $scope.review         = {};
-  $scope.review.author  = $localStorage.user.id;
-  $scope.review.bar     = $scope.bar._id;
-  $scope.active         ='';
-  var review = $scope.review;
-  var rawData = $scope.rawData;
+.controller('ReviewController', ['$scope',
+                                 '$ionicModal',
+                                 '$http',
+                                 '$location',
+                                 '$localStorage',
+                                 '$ionicHistory',
+                                 '$cordovaOauth',
+                                 '$ionicLoading',
+                                 'reviewFactory',
 
-  $scope.createReview   = function() {
-    prepareStats();
+    function($scope, $ionicModal, $http, $location, $localStorage, $ionicHistory, $cordovaOauth, $ionicLoading, reviewFactory){
+      $scope.Math           = window.Math;
+      $scope.rawData        = {};
+      $scope.review         = {};
+      $scope.review.author  = $localStorage.user.id;
+      $scope.review.bar     = $scope.bar._id;
+      $scope.activeAge      ='';
+      $scope.activeCrowd    ='';
+      var review = $scope.review;
+      var rawData = $scope.rawData;
 
-    reviewFactory.create(
-      review)
-      .success(reviewSuccessCallback)
-      .error(reviewErrorCallback);
-  };
+      $scope.createReview   = function() {
+        prepareStats();
 
-  // BUTTONS
-  $scope.setActive = function(type){
-    if (typeof type == 'string') {
-      setCrowdValue(type);
-    } else {
-      setAvgAgeValue(type);
-    }
+        reviewFactory.create(
+          review)
+          .success(reviewSuccessCallback)
+          .error(reviewErrorCallback);
+      };
 
-    $scope.active = type;
-  };
+      // BUTTONS
+      $scope.setActive = function(type){
+        if (typeof type == 'string') {
+          setCrowdValue(type);
+          $scope.activeCrowd = type;
+        } else {
+          setAvgAgeValue(type);
+          $scope.activeAge = type;
+        }
+      };
 
-  $scope.isActive = function(type) {
-    return type === $scope.active;
-  };
+      $scope.isCrowdActive = function (type) {
+        return type === $scope.activeCrowd;
+      };
 
+      $scope.isAgeActive = function(type) {
+        return type === $scope.activeAge;
+      };
 
-  function reviewSuccessCallback (data) {
-    $scope.review = {};
-    $scope.rawData = {};
-    $scope.review.author  = $localStorage.user.id;
-    $scope.review.bar     = $scope.bar._id;
-  }
-
-  function reviewErrorCallback (data, status, headers, config) {
-    alert(data.message);
-  }
-
-
-  function prepareStats () {
-    review.noiseLevel  = rangeConverter(rawData.noiseLevel);
-    review.ggRatio     = parseInt(rawData.ggRatio);
-  }
-
-  function rangeConverter (val) {
-    return ($scope.Math.floor(val/33) + 1);
-  }
+      $scope.buttonInvalid = function () {
+        return $scope.activeCrowd === '' || $scope.activeAge === '';
+      };
 
 
-  //AvgAge Button Data Values
-  function setAvgAgeValue (name) {
-    review.avgAge = name;
-  }
+      function reviewSuccessCallback (data) {
+        $scope.review = {};
+        $scope.rawData = {};
+        $scope.activeAge = '';
+        $scope.activeCrowd = '';
+        $scope.review.author  = $localStorage.user.id;
+        $scope.review.bar     = $scope.bar._id;
+      }
+
+      function reviewErrorCallback (data, status, headers, config) {
+        alert(data.message);
+      }
 
 
-  //CROWD Button Data Values
-  function setCrowdValue (name) {
-    switch(name) {
-      case 'dead':
-        review.crowdLevel = 1;
-        break;
-      case 'ok':
-        review.crowdLevel = 2;
-        break;
-      case 'poppin':
-        review.crowdLevel = 3;
-        break;
-      case 'ugh':
-        review.crowdLevel = 4;
-        break;
-      default:
-        review.crowdLevel = 1;
-        break;
-    }
-  }
+      function prepareStats () {
+        review.noiseLevel  = rangeConverter(rawData.noiseLevel);
+        review.ggRatio     = parseInt(rawData.ggRatio);
+      }
+
+      function rangeConverter (val) {
+        return ($scope.Math.floor(val/33) + 1);
+      }
+
+
+      //AvgAge Button Data Values
+      function setAvgAgeValue (name) {
+        review.avgAge = name;
+      }
+
+
+      //CROWD Button Data Values
+      function setCrowdValue (name) {
+        switch(name) {
+          case 'dead':
+            review.crowdLevel = 1;
+            break;
+          case 'ok':
+            review.crowdLevel = 2;
+            break;
+          case 'poppin':
+            review.crowdLevel = 3;
+            break;
+          case 'ugh':
+            review.crowdLevel = 4;
+            break;
+          default:
+            review.crowdLevel = 1;
+            break;
+        }
+      }
 }]);
