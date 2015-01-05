35ca0d492b2d0c4559796fc6c599fee8041c04f9
Neil Lambert <njlambert@gmail.com>
2015-01-05 15:49:34 -0800

    Update settingsModel.js

--- src/user_settings/settingsModel.js
+++ src/user_settings/settingsModel.js
@@ -6,8 +6,6 @@ angular.module('settingsModel', [])
   		return $http.get('http://127.0.0.1:3000/api/users/' + $localStorage.user.id + '/settings');
   	},
     update 	: function(newSettings, userId) {
-    	console.log(userId);
-    	console.log($localStorage.user.id);
       return $http.put('http://127.0.0.1:3000/api/users/' + $localStorage.user.id + '/settings', newSettings);
     }
   };
