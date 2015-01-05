2288a4046e968918fc286e94bdd223043fbef239
neilcode <njlambert@gmail.com>
2015-01-05 15:32:01 -0800

    fixed user settings not being retrieved from server properly.

--- src/user_settings/settingsCtrl.js
+++ src/user_settings/settingsCtrl.js
@@ -3,9 +3,9 @@ angular.module('userSettingsCtrl', [])
 .controller('SettingsController', ['$scope', '$localStorage', '$ionicHistory', '$cordovaOauth', '$ionicLoading', 'settingsFactory', 'userSettings', function($scope, $localStorage, $ionicHistory, $cordovaOauth, $ionicLoading, settingsFactory, userSettings){
 	$scope.user = $localStorage.user;
 	$scope.newSettings = {
-		radiusDefinition : userSettings.searchRadius //grabbed from the server 
+		radiusDefinition : userSettings.data.searchRadius || 10//grabbed from the server 
 	};
-
+	
 	$scope.updateSettings = function(){
 		settingsFactory.update($scope.newSettings, $scope.user.id)
 		.success(updateSuccessCallback)
@@ -14,7 +14,7 @@ angular.module('userSettingsCtrl', [])
 
 
   function updateSuccessCallback (data) {
-  	
+  	console.log("data", data);
   };
 
   function updateErrorCallback (data, status, headers, config) {
--- src/user_settings/settingsModel.js
+++ src/user_settings/settingsModel.js
@@ -6,7 +6,9 @@ angular.module('settingsModel', [])
   		return $http.get('http://127.0.0.1:3000/api/users/' + $localStorage.user.id + '/settings');
   	},
     update 	: function(newSettings, userId) {
-      return $http.put('http://127.0.0.1:3000/api/users/' + userId + '/settings', newSettings);
+    	console.log(userId);
+    	console.log($localStorage.user.id);
+      return $http.put('http://127.0.0.1:3000/api/users/' + $localStorage.user.id + '/settings', newSettings);
     }
   };
 });
