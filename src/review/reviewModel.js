56f6220838d138ea4a05eee1bbf7a366aca54f04
Sharmila Jesupaul <tosharmila@gmail.com>
2015-01-05 14:48:51 -0800

    Make Review Routes RESTful: - changes review factory methods to reflect restful routes - changes review controller to reflect restful route info

--- src/review/reviewModel.js
+++ src/review/reviewModel.js
@@ -2,14 +2,14 @@ angular.module('reviewModel', [])
 
 .factory('reviewFactory', function($localStorage, $location, $http) {
   return {
-    create: function(reviewData) {
-      return $http.post('http://127.0.0.1:3000/api/reviews', reviewData);
+    create: function(barId, reviewData) {
+      return $http.post('http://127.0.0.1:3000/api/bars/' + barId + '/reviews', reviewData);
     },
     get   : function(barId) {
-      return $http.get('http://127.0.0.1:3000/api/reviews/' + barId);
+      return $http.get('http://127.0.0.1:3000/api/bars/' + barId + '/reviews');
     },
-    update: function(barId, reviewData) {
-      return $http.put('http://127.0.0.1:3000/api/reviews/' + barId, reviewData);
+    update: function(barId, reviewId, reviewData) {
+      return $http.put('http://127.0.0.1:3000/api/bars/' + barId + '/reviews/' + reviewId, reviewData);
     }
   };
 });
