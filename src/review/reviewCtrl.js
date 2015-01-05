cf89173fa0042a21b6ec8ab9327c04d469bfae14
Sharmila Jesupaul <tosharmila@gmail.com>
2015-01-04 20:46:02 -0800

    Implement Review Updates: - form submission and submit button is toggled when user makes first review - review controller updates reviews - add put route to review factory

--- src/review/reviewCtrl.js
+++ src/review/reviewCtrl.js
@@ -23,6 +23,7 @@ angular.module('reviewCtrl', [])
       $scope.submissionText = 'Submit';
 
 
+
       $scope.formSubmission = function () {
         if(toggleBool) {
           toggleBool = !toggleBool;
@@ -72,6 +73,15 @@ angular.module('reviewCtrl', [])
         return $scope.activeCrowd === '' || $scope.activeAge === '';
       };
 
+      function reviewUpdateSuccess (data) {
+        $scope.review.author  = $localStorage.user.id;
+        $scope.review.bar     = $scope.bar._id;
+      }
+
+      function reviewUpdateError (data, status, headers, config) {
+        alert(data.message);
+      }
+
 
       function reviewUpdateSuccess (data) {
         $scope.review.author  = $localStorage.user.id;
@@ -91,7 +101,6 @@ angular.module('reviewCtrl', [])
       function reviewErrorCallback (data, status, headers, config) {
         alert(data.message);
       }
-
 
       function prepareStats () {
         review.noiseLevel  = rangeConverter(rawData.noiseLevel);
--- src/review/reviewModel.js
+++ src/review/reviewModel.js
@@ -2,11 +2,14 @@ angular.module('reviewModel', [])
 
 .factory('reviewFactory', function($localStorage, $location, $http) {
   return {
-    create 	: function(reviewData) {
+    create: function(reviewData) {
       return $http.post('http://127.0.0.1:3000/api/reviews', reviewData);
     },
-    get 		: function(barId) {
-    	return $http.get('http://127.0.0.1:3000/api/reviews/' + barId);
+    get   : function(barId) {
+      return $http.get('http://127.0.0.1:3000/api/reviews/' + barId);
+    },
+    update: function(barId, reviewData) {
+      return $http.put('http://127.0.0.1:3000/api/reviews/' + barId, reviewData);
     }
   };
 });
