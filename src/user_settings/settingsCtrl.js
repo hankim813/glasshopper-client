a92284bcb6eea04de3bb5179e39c5f276eda5c69
neilcode <njlambert@gmail.com>
2015-01-05 17:25:28 -0800

    adds a placeholder profile image for users with no Facebook OAuth

--- src/user_settings/settingsCtrl.js
+++ src/user_settings/settingsCtrl.js
@@ -5,7 +5,7 @@ angular.module('userSettingsCtrl', [])
 	$scope.newSettings = {
 		radiusDefinition : userSettings.data.searchRadius || 10//grabbed from the server 
 	};
-	
+	console.log("User: ",$scope.user);
 	$scope.updateSettings = function(){
 		settingsFactory.update($scope.newSettings, $scope.user.id)
 		.success(updateSuccessCallback)
