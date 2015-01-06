e90934504d0ec391933da09a7b395da09813681e
neilcode <njlambert@gmail.com>
2015-01-05 21:52:47 -0800

    adds a more granular 0.1 - 10.0 slider for setting user default search radius.

--- src/user_settings/settingsCtrl.js
+++ src/user_settings/settingsCtrl.js
@@ -3,10 +3,11 @@ angular.module('userSettingsCtrl', [])
 .controller('SettingsController', ['$scope', '$localStorage', '$ionicHistory', '$cordovaOauth', '$ionicLoading', 'settingsFactory', 'userSettings', function($scope, $localStorage, $ionicHistory, $cordovaOauth, $ionicLoading, settingsFactory, userSettings){
 	$scope.user = $localStorage.user;
 	$scope.newSettings = {
-		radiusDefinition : userSettings.data.searchRadius || 10//grabbed from the server 
+		radiusDefinition : userSettings.data.searchRadius || 0.1//grabbed from the server 
 	};
 	console.log("User: ",$scope.user);
-	$scope.updateSettings = function(){
+	$scope.updateSettings = function() {
+		console.log("newSettings", $scope.newSettings);
 		settingsFactory.update($scope.newSettings, $scope.user.id)
 		.success(updateSuccessCallback)
 		.error(updateErrorCallback);
