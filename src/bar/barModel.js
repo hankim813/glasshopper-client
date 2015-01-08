80e7358b6eb0bfc31d92499cfd9c5245834851cd
Stefano Schiavi <stefano@bvprojects.org>
2015-01-08 09:40:27 -0800

    make nearby method execute 2 api calls

--- src/bar/barModel.js
+++ src/bar/barModel.js
@@ -1,17 +1,27 @@
 angular.module('barModel', [])
 
-.factory('barFactory', function($localStorage, $location, $http) {
+.factory('barFactory', function($localStorage, $location, $http, $timeout) {
   return {
 
     findNearby: function(lng,lat,radius) {
       return $http(
-                  { url         : 'http://127.0.0.1:3000/api/bars/nearby',
-                    method      : 'GET',
-                    params      :
-                      { lng     : lng,
-                        lat     : lat,
-                        radius  : radius }
-      });
+              { url         : 'http://127.0.0.1:3000/api/bars/googlefetch',
+                method      : 'GET',
+                params      :
+                  { lng     : lng,
+                    lat     : lat,
+                    radius  : radius }
+      }).then(
+        function() {
+          return $http(
+                      { url         : 'http://127.0.0.1:3000/api/bars/nearby',
+                        method      : 'GET',
+                        params      :
+                          { lng     : lng,
+                            lat     : lat,
+                            radius  : radius }
+          });
+        })
     },
 
     get: function(barId) {
