610126f469e2ffa2a451ac63b5bbef156d16b0c9
neilcode <njlambert@gmail.com>
2015-01-08 19:59:58 -0800

    FINISHED. FINISHED ALL THOSE FUCKERS.

--- src/bar/barCtrl.js
+++ src/bar/barCtrl.js
@@ -1,6 +1,6 @@
-angular.module('barCtrl', ['ionic']).
+angular.module('barCtrl', ['ionic'])
 
-controller('BarController', function($scope, $http, $location, $ionicHistory, $localStorage, $ionicLoading, barFactory, SearchData){
+.controller('BarController', function($scope, $http, $location, $ionicHistory, $localStorage, $ionicLoading, barFactory, SearchData){
   function init () {
         if (SearchData.getCoords()) {
             barFactory.findNearby(SearchData.getCoords().lng, SearchData.getCoords().lat, $localStorage.user.searchRadius).then(function(response) {
@@ -120,11 +120,11 @@ function shoveIntoArray (bar) {
     // VISUALIZATIONS
   $scope.calculateNoise = function() {
     //determine the colors for our volume bar graph
-    var noiseLevel = 0;
+    var noiseLevel = 1;
     if ($scope.aggregates.noiseLevel) {
       noiseLevel = $scope.aggregates.noiseLevel;
     } else {
-      noiseLevel = 0;
+      noiseLevel = 1;
     }
 
     var fillColors = ["rgb(255, 158, 0)",
@@ -143,18 +143,18 @@ function shoveIntoArray (bar) {
 
   $scope.calculateAge = function() {
     //determine the colors for our volume bar graph
-    var ageLevel = 0;
-    if ($scope.aggregates.ageLevel) {
-      ageLevel = $scope.aggregates.ageLevel;
+    var ageLevel = 1;
+    if ($scope.aggregates.avgAge) {
+      ageLevel = $scope.aggregates.avgAve;
     } else {
-      ageLevel = 0;
+      ageLevel = 1;
     }
 
-    var fillColors = ["rgb(255, 158, 0)",
-                      "rgb(255, 94, 0)",
-                      "rgb(232, 123, 12)",
-                      "rgb(232, 63, 12)",
-                      "rgb(255, 41, 17)"];
+    var fillColors = ["rgb(79, 247, 194)",
+                      "rgb(66, 214, 210)",
+                      "rgb(80, 200, 237)",
+                      "rgb(70, 112, 247)",
+                      "rgb(8, 111, 214)"];
     for (i = 1; i < fillColors.length; i ++ ) {
       if (i >= ageLevel) {
         fillColors[i] = "rgb(214,214,214)"; //faded blue
@@ -275,7 +275,8 @@ function shoveIntoArray (bar) {
   // Can't check in unless you are you at least 200ft away from the bar
 
   (function(){
-    if ($stateParams.distance > 0.04) { //CHECK_IN_RADIUS
+    $scope.visualize();
+    if ($stateParams.distance > 0.1) { //CHECK_IN_RADIUS
 
       $scope.ifNotNearBy = true;
       $scope.checkinButtonMsg = "Too far away to check in!";
